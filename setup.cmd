1f4aa9783ab77560966810a44604fcea5ca248d1
couleurm <couleur@riseup.net>
2021-07-12 00:27:26 +0200

    cls clean

--- setup.cmd
+++ setup.cmd
@@ -1,8 +1,11 @@
 @echo off
+cls
 echo Downloading OBS [47.7MB]
 powershell Invoke-WebRequest "https://github.com/couleurm/obs-portable/archive/refs/heads/main.zip" -OutFile "$env:homedrive$env:homepath\AppData\Local\Temp\OBS-portable.zip"
+cls
 echo Download Done! Unzipping..
 powershell Expand-Archive -Path "C:$env:homepath\AppData\Local\Temp\OBS-portable.zip" -D "$env:homedrive$env:homepath\Documents"
+cls
 echo Download Done! Launching the OBS profile maker..
 timeout 2 >nul
 cls
